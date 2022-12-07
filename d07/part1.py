c2a4956b62d2cea5e1bbe3da00012dadf6ecd444
David McFadzean <davidmc@gmail.com>
2022-12-07 13:15:56 -0500

    Day 7 checkpoint

---
+++ d07/part1.py
@@ -0,0 +1,62 @@
+data = """$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"""
+
+lines = data.split('\n')
+
+cwd = []
+fs = {}
+
+def add2fs(fs, path, name, val):    
+    d = fs
+    for key in path:
+        d = d.get(key)
+    d[name] = val
+    print(d)
+
+for line in lines:
+    print(line)
+    if line[0] == '$':
+        cmd = line[2:]
+        print(cmd)
+        if cmd[:2] == 'cd':
+            dir = cmd[3:]
+            if dir == '/':
+                cwd = []
+            elif dir == '..':
+                cwd.pop()
+            else:
+                cwd.append(dir)
+            print("cwd", cwd)
+    else:
+        if line[:3] == 'dir':
+            dir = line[4:]
+            print("add dir", dir)
+            add2fs(fs, cwd, dir, {})
+        else:
+            size, name = line.split(' ')
+            size = int(size)
+            print("add file", name, "with size", size)
+            add2fs(fs, cwd, name, size)
+print(fs)
+
