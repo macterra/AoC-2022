96ae4e224cb230002e8edc5f440361d29f49944a
David McFadzean <davidmc@gmail.com>
2022-12-10 11:18:57 -0500

    Day 10

---
+++ d10/part2.py
@@ -0,0 +1,183 @@
+import numpy as np
+
+data = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop"""
+
+data = open('data', 'r').read()
+lines = data.split('\n')
+
+X = 1
+cycle = 1
+cycles = [X]
+
+for line in lines:
+    if line[:4] == 'noop':
+        cycles.append(X)
+        cycle += 1
+    else:
+        cmd, val = line.split(' ')
+        val = int(val)
+        cycles.append(X)
+        cycles.append(X)
+        cycle += 2
+        X += val
+
+screen = np.full((6,40), ".")
+
+for i in range(1, len(cycles)):
+    c = i-1    
+    row = int(c/40)
+    col = c%40
+    pos = cycles[i]
+    diff = col-pos
+    on = (diff == -1) or (diff == 0) or (diff == 1)
+    #print(i, row, col, pos, diff, on)
+    if on:
+        screen[row,col] = '#'
+
+for row in range(6):
+    print("".join(screen[row,:]))
