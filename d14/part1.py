8b299e087076ebd6d1b7bda0ffcf904c449604aa
David McFadzean <davidmc@gmail.com>
2022-12-14 12:03:44 -0500

    Day 14 part 1

--- d14/part1.py
+++ d14/part1.py
@@ -28,20 +28,19 @@ def makeGrid(paths):
             miny = min(miny, y)
 
     print(rows)
-    grid = np.full((rows+1,cols+1), '.')
+    grid = np.full((rows+1,cols+2), '.')
 
     for path in paths:
         for i in range(len(path)-1):
             x1, y1 = path[i]
             x2, y2 = path[i+1]
-            print("from {}:{} to {}:{}".format(x1, y1, x2, y2))
-
             x1, x2 = min(x1, x2), max(x1, x2)
             y1, y2 = min(y1, y2), max(y1, y2)
             grid[y1:y2+1,x1:x2+1] = '#'
 
     return grid, minx, miny
 
+data = open('data', 'r').read()
 lines = data.split('\n')
 paths = parse(lines)
 grid, minx, miny = makeGrid(paths)
@@ -50,6 +49,37 @@ print(minx, miny)
 
 grid[0:1,500:501] = '+'
 
-w,h = grid.shape
-for row in range(w):
-    print(row, "".join(grid[row,:][minx:]))
+def printGrid(grid, minx):
+    w,h = grid.shape
+    for row in range(w):
+        print(row, "".join(grid[row,:][minx:]))
+
+def drop(grid, x, y):
+    h, w = grid.shape
+    #print("drop", x, y, w, h)
+    y += 1
+
+    if y+1 == h:
+        return False
+
+    #print("grid at x,y = ", grid[y:y+1,x:x+1])
+
+    if grid[y+1:y+2,x:x+1] == '.':
+        return drop(grid, x, y)
+
+    if grid[y+1:y+2,x-1:x] == '.':
+        return drop(grid, x-1, y)
+
+    if grid[y+1:y+2,x+1:x+2] == '.':
+        return drop(grid, x+1, y)
+
+    grid[y:y+1,x:x+1] = 'o'    
+    printGrid(grid, minx)
+    return True
+
+grains = 0
+while drop(grid, 500, 0):
+    grains += 1
+
+printGrid(grid, minx)
+print(grains)
