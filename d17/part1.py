cabf28aead99ee4714d58a3c7ca54ceb7b76012f
David McFadzean <davidmc@gmail.com>
2022-12-19 11:18:16 -0500

    Day 17 checkpoint

--- d17/part1.py
+++ d17/part1.py
@@ -33,51 +33,74 @@ def makeRock(w, h, pattern):
 
 data = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
 
+#data = open('data', 'r').read()
+jets = list(data)
+
 rocks = []
 rocks.append(makeRock(4,1,"####"))
 rocks.append(makeRock(3,3,".#.###.#."))
 rocks.append(makeRock(3,3,"..#..#####"))
 rocks.append(makeRock(1,4,"####"))
 rocks.append(makeRock(2,2,"####"))
-#print(rocks)
 
-chamber = np.full((3,7), 0)
-top = 0
-t = 0
-
-def addRock(rock, top, chamber, rocks):
-    print(rock)
-    h, w = rock.shape
-    ht, _ = chamber.shape
-
-    print(w, h, ht, top)
-    if ht <= top + h + 3:
-        chamber = np.vstack((np.full((h,7),0), chamber))
-    
-    return chamber
-
-# rock = np.copy(rocks[0])
-# chamber = addRock(rock, top, chamber, rocks)
-# print(chamber)
-
-# for rock in rocks:
-#     chamber = np.vstack((rock, chamber))
-
-print(chamber)
-print()
-print(rocks[1])
-print()
-print(chamber[6:9,])
-print()
-test = rocks[1] + chamber[6:9,] == 2
-print(test, np.any(test))
-
-def shift(rock, left):
-    if left:
+def shift(rock, jet):
+    if jet == '<':
         if np.all(rock[:,0] == 0):
             rock = np.roll(rock, -1, axis=1)
-    else:
+    elif jet == '>':
         if np.all(rock[:,-1] == 0):
             rock = np.roll(rock, 1, axis=1)
+    else:
+        exit()
     return rock
 
+template = np.full((20,7), 0)
+chamber = np.copy(template)
+
+def nextRock():
+    rock = rocks.pop(0)
+    rocks.append(rock)
+    return np.copy(rock)
+
+def nextJet():
+    jet = jets.pop(0)
+    jets.append(jet)
+    return jet
+
+def findTop(chamber):
+    ht, _ = chamber.shape
+    for row in range(ht):
+        if np.any(chamber[row,:] == 1):
+            break
+    return row
+
+def drop(chamber):
+    top = findTop(chamber)
+    
+    if top < 20:
+        chamber = np.vstack((template, chamber))
+        top += 20
+
+    ht, _ = chamber.shape
+    alt = top - 3
+    rock = nextRock()
+    while alt < ht:
+        jet = nextJet()
+        shifted = shift(rock, jet)
+        test = shifted * chamber[alt-4:alt,] == 1
+        if not np.any(test):
+            rock = shifted
+        test = rock * chamber[alt-3:alt+1,] == 1
+        if np.any(test):
+            break
+        alt += 1
+        
+    chamber[alt-4:alt,] += rock
+    #print(chamber)
+    return chamber
+
+for i in range(2022):
+    chamber = drop(chamber)
+    ht, _ = chamber.shape
+    top = findTop(chamber)
+    print(i, ht, top, ht-top)
