b887dfb6386e770ad96c0c98393ba6c7c9ff98a6
David McFadzean <davidmc@gmail.com>
2022-12-21 10:38:55 -0500

    Day 21 part 1

---
+++ d21/part1.py
@@ -0,0 +1,56 @@
+data = """root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32"""
+
+class Monkey:
+    def __init__(self, line):
+        name, spec = line.split(': ')
+        self.name = name
+        self.spec = spec
+
+    def yell(self, monkeys):
+        print(self.name, len(self.spec))
+
+        if len(self.spec) == 11:
+            m1, op, m2 = self.spec.split(' ')
+
+            r1 = monkeys[m1].yell(monkeys)
+            r2 = monkeys[m2].yell(monkeys)
+            
+            if op == '+':
+                return r1 + r2
+            elif op == '-':
+                return r1 - r2
+            elif op == '*':
+                return r1 * r2
+            elif op == '/':
+                return int(r1 / r2)
+            else:
+                exit()
+
+        return int(self.spec)
+        
+def parse(lines):
+    monkeys = {}
+    for line in lines:
+        monkey = Monkey(line)
+        monkeys[monkey.name] = monkey
+    return monkeys
+
+data = open('data', 'r').read()
+lines = data.split('\n')
+monkeys = parse(lines)
+
+print(monkeys['root'].yell(monkeys))
